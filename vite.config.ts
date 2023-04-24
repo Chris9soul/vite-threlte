330f08a50d06aa031981c4b643d8752b7ed56eb4
Chris <85393398+Chris9soul@users.noreply.github.com>
2023-04-24 15:39:55 +0300

    init

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  ssr: {
+		noExternal: ['three', 'troika-three-text']
+	},
+  build:
+    {
+        outDir: './dist',
+        emptyOutDir: true,
+        sourcemap: true,
+        rollupOptions: {
+            output: {
+              entryFileNames: `assets/[name].js`,
+              chunkFileNames: `assets/[name].js`,
+              assetFileNames: `assets/[name].[ext]`,
+              format: 'iife'
+            }
+          }
+    }
+})
