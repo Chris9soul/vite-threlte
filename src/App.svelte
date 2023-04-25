6f6aaf9296c4a75cea6197057a92b8066e79d670
Chris <85393398+Chris9soul@users.noreply.github.com>
2023-04-25 04:38:53 +0300

    Revert "fix version issues"
    This reverts commit ce59a8f5039b67d167d9b4c1a4fc3424a841b246.

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,52 @@
-<script lang="ts">
-	import { Canvas } from '@threlte/core'
-	import { HTML } from '@threlte/extras'
-	import { World, Debug } from '@threlte/rapier'
-	import AdvancedScene from './AdvancedScene.svelte'
+<script>
+  import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core'
+  import { spring } from 'svelte/motion'
+  import { degToRad } from 'three/src/math/MathUtils'
 
+  const scale = spring(1)
 </script>
 
-<Canvas>
-	<World gravity={{ y: 0 }}>
-    <AdvancedScene type={'static'}  />
-		<HTML slot="fallback" transform>
-			<p>
-				It seems your browser<br />
-				doesn't support WASM.<br />
-				I'm sorry.
-			</p>
-		</HTML>
-	</World>
-</Canvas>
+<div>
+  <Canvas>
+    <T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
+      <OrbitControls
+        maxPolarAngle={degToRad(80)}
+        enableZoom={false}
+        target={{ y: 0.5 }}
+      />
+    </T.PerspectiveCamera>
+
+    <T.DirectionalLight castShadow position={[3, 10, 10]} />
+    <T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
+    <T.AmbientLight intensity={0.2} />
+
+    <!-- Cube -->
+    <T.Group scale={$scale}>
+      <T.Mesh position.y={0.5} castShadow let:ref>
+        <!-- Add interaction -->
+        <InteractiveObject
+          object={ref}
+          interactive
+          on:pointerenter={() => ($scale = 2)}
+          on:pointerleave={() => ($scale = 1)}
+        />
+
+        <T.BoxGeometry />
+        <T.MeshStandardMaterial color="#333333" />
+      </T.Mesh>
+    </T.Group>
+
+    <!-- Floor -->
+    <T.Mesh receiveShadow rotation.x={degToRad(-90)}>
+      <T.CircleGeometry args={[3, 72]} />
+      <T.MeshStandardMaterial color="white" />
+    </T.Mesh>
+  </Canvas>
+</div>
 
 <style>
-	p {
-		font-size: 0.75rem;
-		line-height: 1rem;
-	}
+  div {
+    height: 100%;
+    width: 100%;
+  }
 </style>
